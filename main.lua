3ab41f488d16b334682ecd3430c853df17cb987e
Kakich2 <131226367+Kakich2@users.noreply.github.com>
2025-02-27 20:55:32 +0700

    Добавления тестового шаблона генерации мира
    1) Модифицирован world_gen.lua - добавлена функция генерации мира
    2) Добавлена отрисовка мира

--- main.lua
+++ main.lua
@@ -9,20 +9,40 @@ function ComparePoints(point_1, point_2) --Необходимо для сорт�
     end
 end
 
+
+function IterateWorldDrawing(world)
+    for i = 1, #world do
+        local world_level_x = world[i]
+        for j = 1, #world_level_x do
+            if (world_level_x[j] == 0) then
+                love.graphics.setColor(0, 0, 0)
+            end
+            if (world_level_x[j] == 1) then
+                love.graphics.setColor(1, 1, 1)
+            end
+            if (world_level_x[j] == 2) then
+                love.graphics.setColor(1, 0.5, 0.5)
+            end
+            love.graphics.rectangle('fill', (j - 1) * WORLD_CUBE_SIZE, (i - 1) * WORLD_CUBE_SIZE, WORLD_CUBE_SIZE, WORLD_CUBE_SIZE)
+        end
+    end
+end
+
 function love.load()
     love.math.setRandomSeed(os.time())
 
-    local WORLD_HEIGHT = 600
-    local WORLD_WIDTH = 800
+    local WORLD_HEIGHT = 60
+    local WORLD_WIDTH = 80
+    WORLD_CUBE_SIZE = 10
     local WORLD_VORONOI_POINTS_COUNT = 5
 
     WORLD_VORONOI_POINTS = world_gen:CreatePoints(WORLD_WIDTH, WORLD_HEIGHT, WORLD_VORONOI_POINTS_COUNT) -- Генерация точек для генерации шума (WIP)
+    WORLD_BLOCKS = world_gen:CreateWorld(WORLD_WIDTH, WORLD_HEIGHT, "TEST_BLANK_HALF_SEA")
 
     table.sort(WORLD_VORONOI_POINTS, ComparePoints) -- Собственно сортировка точек по координате X
-
 end
 
-function love.update(dt)
+function love.update(dt) 
 end
 
 function love.draw()
@@ -30,4 +50,5 @@ function love.draw()
     for i = 1, #WORLD_VORONOI_POINTS do
         love.graphics.points(WORLD_VORONOI_POINTS[i])
     end
+    IterateWorldDrawing(WORLD_BLOCKS)
 end
--- world_gen.lua
+++ world_gen.lua
@@ -1,6 +1,10 @@
 -- world_gen.lua
 -- Библиотека для генерации мира
 
+-- ID блоков:
+-- 1 - Белый блокозаменитель
+-- 2 - Розовый блокозаменитель
+
 local world_gen = {}
 
 function world_gen:CreatePoints(WORLD_WIDTH, WORLD_HEIGHT, POINTS_COOUNT) 
@@ -12,6 +16,27 @@ function world_gen:CreatePoints(WORLD_WIDTH, WORLD_HEIGHT, POINTS_COOUNT)
         }) 
     end
     return POINTS
+end
+
+function world_gen:CreateWorld(WORLD_WIDTH, WORLD_HEIGHT, GENERATION_TYPE)
+    local WORLD_BLOCKS_ID = {}
+    if GENERATION_TYPE == "TEST_BLANK_HALF_SEA" then
+        for i = 1, WORLD_HEIGHT do
+            local WORLD_X_LEVEL = {}
+            for j = 1, WORLD_WIDTH do
+                local BLOCK_ID = 0
+                if i > (WORLD_HEIGHT / 2) then
+                    BLOCK_ID = 1
+                    if j % 2 == 0 then
+                        BLOCK_ID = 2
+                    end
+                end
+                table.insert(WORLD_X_LEVEL, BLOCK_ID)
+            end
+            table.insert(WORLD_BLOCKS_ID, WORLD_X_LEVEL)
+        end
+    end
+    return WORLD_BLOCKS_ID
 end
 
 return world_gen
